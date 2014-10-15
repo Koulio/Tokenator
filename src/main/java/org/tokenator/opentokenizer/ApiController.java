2c68f4714ac6410a39ab5e7adf6547e2e78fb2a7
Дмитрий Холодов <github@sec.to>
2014-10-15 15:31:53 -0500

    added delete methods and more error handling

--- src/main/java/org/tokenator/opentokenizer/ApiController.java
+++ src/main/java/org/tokenator/opentokenizer/ApiController.java
@@ -1,12 +1,15 @@
 package org.tokenator.opentokenizer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.tokenator.opentokenizer.domain.entity.PrimaryData;
 import org.tokenator.opentokenizer.domain.entity.SurrogateData;
 import org.tokenator.opentokenizer.domain.repository.PrimaryDataRepository;
+import org.tokenator.opentokenizer.domain.repository.SurrogateDataRepository;
 
 import javax.transaction.Transactional;
 import java.util.Date;
@@ -18,10 +21,15 @@ import static org.tokenator.opentokenizer.util.DateSerializer.DATE_FORMAT;
 public class ApiController {
 
     private PrimaryDataRepository primaryDataRepo;
+    private SurrogateDataRepository surrogateDataRepo;
 
     @Autowired
-    public ApiController(PrimaryDataRepository primaryDataRepo) {
+    public ApiController(
+            PrimaryDataRepository primaryDataRepo,
+            SurrogateDataRepository surrogateDataRepo
+    ) {
         this.primaryDataRepo = primaryDataRepo;
+        this.surrogateDataRepo = surrogateDataRepo;
     }
 
 
@@ -41,19 +49,24 @@ public class ApiController {
     }
 
 
-    /*
-     *  Lookup primary PAN data by id. Example retrieving primary data for id=1:
-     *
-     *   $ curl -X GET http://localhost:8080/api/v1/primaries/1
-     */
+   /*
+    *  Lookup primary PAN data by id. Example retrieving primary data for id=1:
+    *
+    *   $ curl -X GET http://localhost:8080/api/v1/primaries/1
+    */
     @RequestMapping(
             value = "/primaries/{primaryId}",
             method = RequestMethod.GET
     )
     @ResponseStatus(HttpStatus.CREATED)
     public PrimaryData findPrimaryById(@PathVariable(value="primaryId") Long primaryId) {
-        return primaryDataRepo.findOne(primaryId);
+        PrimaryData primary = primaryDataRepo.findOne(primaryId);
+        if (primary == null) {
+            throw new EntityNotFoundException(PrimaryData.class, primaryId);
+        }
+        return primary;
     }
+
 
     /*
      *  Lookup primary data by pan and yyMM expiration date.  Example:
@@ -64,13 +77,34 @@ public class ApiController {
             value = "/primaries/{primaryPan}/{primaryExpr}",
             method = RequestMethod.GET
     )
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     public PrimaryData findPrimaryByPanAndExpr(
             @PathVariable(value="primaryPan") String primaryPan,
             @PathVariable(value="primaryExpr")
             @DateTimeFormat(pattern = DATE_FORMAT) Date primaryExpr) {
-        return primaryDataRepo.findByPanAndExpr(primaryPan, primaryExpr);
+
+        PrimaryData primary = primaryDataRepo.findByPanAndExpr(primaryPan, primaryExpr);
+        if (primary == null) {
+            throw new EntityNotFoundException(PrimaryData.class, primaryPan, primaryExpr);
+        }
+        return primary;
     }
+
+    /*
+     *  Delete primary PAN data by id.  Surrogates are deleted by cascade. Example
+     *
+     *   $ curl -X DELETE http://localhost:8080/api/v1/primaries/1
+     */
+    @RequestMapping(
+            value = "/primaries/{primaryId}",
+            method = RequestMethod.DELETE
+    )
+    @ResponseStatus(HttpStatus.NO_CONTENT) /* 204, success but no response payload */
+    public void deletePrimaryById(@PathVariable(value="primaryId") long primaryId) {
+        primaryDataRepo.delete(primaryId);
+    }
+
+
 
     /*
      *  Create a surrogate for a primary data entry with the specified id.  Example:
@@ -87,10 +121,14 @@ public class ApiController {
     public PrimaryData createSurrogate(
             @PathVariable(value="primaryId") Long primaryId,
             @RequestBody SurrogateData surrogateData) {
+
         PrimaryData primary = primaryDataRepo.findOne(primaryId);
-        if (primary != null) {
-            primary.addSurrogate(surrogateData);
+        if (primary == null) {
+            throw new EntityNotFoundException(PrimaryData.class, primaryId);
         }
+
+        primary.addSurrogate(surrogateData);
+
         return primary;
     }
 
@@ -104,11 +142,43 @@ public class ApiController {
             value = "/primaries/surrogates/{surrogatePan}/{surrogateExpr}",
             method = RequestMethod.GET
     )
+    @ResponseStatus(HttpStatus.OK)
     public PrimaryData findPrimaryOfSurrogate(
             @PathVariable(value="surrogatePan") String surrogatePan,
             @PathVariable(value="surrogateExpr")
             @DateTimeFormat(pattern = DATE_FORMAT) Date surrogateExpr) {
-        return primaryDataRepo.findBySurrogate(surrogatePan, surrogateExpr);
+        PrimaryData primary = primaryDataRepo.findBySurrogate(surrogatePan, surrogateExpr);
+        if (primary == null) {
+            throw new EntityNotFoundException(SurrogateData.class, surrogatePan, surrogateExpr);
+        }
+        return primary;
+    }
+
+
+   /*
+    *  Delete surrogate PAN data by id. Example
+    *
+    *   $ curl -X DELETE http://localhost:8080/api/v1/surrogates/1
+    */
+    @RequestMapping(
+            value = "/surrogates/{surrogateId}",
+            method = RequestMethod.DELETE
+    )
+    @ResponseStatus(HttpStatus.NO_CONTENT) /* 204, success but no response payload */
+    @Transactional
+    public void deleteSurrogate(@PathVariable(value="surrogateId") long surrogateId) {
+        surrogateDataRepo.delete(surrogateId);
+    }
+
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    @ResponseStatus(value=HttpStatus.NOT_FOUND,reason="Entity not found")
+    public void notFound() {
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(value=HttpStatus.CONFLICT,reason="Entity already exists")
+    public void duplicateEntryExists() {
     }
 
 }
